37a26e492bfafcafe6705d791db46abf3969298c
sndp231998 <sandipchapagain797@gmail.com>
2025-04-13 16:05:50 +0545

    first setup

---
+++ src/main/java/com/arinax/ArenaXApplication.java
@@ -0,0 +1,13 @@
+package com.arinax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArenaXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArenaXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arinax/ArenaXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arinax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArenaXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
